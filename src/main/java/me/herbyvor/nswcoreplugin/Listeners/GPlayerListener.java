cac6436d6306e7f7cb6fa4aa1b2bd7acbd7b4d15
jeanfrancoisemil <50072047+jeanfrancoisemil@users.noreply.github.com>
2022-04-24 23:39:22 +0200

    fixes + debug, add d'un listener

---
+++ src/main/java/me/herbyvor/nswcoreplugin/Listeners/GPlayerListener.java
@@ -0,0 +1,27 @@
+package me.herbyvor.nswcoreplugin.Listeners;
+
+import com.google.common.util.concurrent.Service;
+import me.herbyvor.nswcoreplugin.NswCorePlugin;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class GPlayerListener implements Listener {
+
+    private NswCorePlugin main;
+
+    public GPlayerListener(NswCorePlugin main1){this.main = main1;}
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+
+
+        Player p = event.getPlayer();
+
+        main.VoteNumber.put(p,1);
+
+
+    }
+
+}
--- src/main/java/me/herbyvor/nswcoreplugin/NswCorePlugin.java
+++ src/main/java/me/herbyvor/nswcoreplugin/NswCorePlugin.java
@@ -19,6 +19,7 @@ public final class NswCorePlugin extends JavaPlugin {
 
     public HashMap<Player, Integer> VoteNumber;
 
+
     @Override
     public void onEnable() {
 
