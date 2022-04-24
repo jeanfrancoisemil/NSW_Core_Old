12451ec217491bdb5791cc99ae7e7bda9e78960c
jeanfrancoisemil <50072047+jeanfrancoisemil@users.noreply.github.com>
2022-04-25 00:08:25 +0200

    ajout d'une alternative au bookopening pour le /vote

--- src/main/java/me/herbyvor/nswcoreplugin/Commands/VoteCommand.java
+++ src/main/java/me/herbyvor/nswcoreplugin/Commands/VoteCommand.java
@@ -10,6 +10,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
 
+import java.awt.*;
+
 import static jdk.javadoc.internal.doclets.formats.html.markup.HtmlStyle.title;
 
 public class VoteCommand implements CommandExecutor {
@@ -25,10 +27,7 @@ public class VoteCommand implements CommandExecutor {
 
             Player p = (Player) sender;
 
-
-            main.VoteNumber.merge(p, 1, Integer::sum);
-
-            if (main.VoteNumber.get(p) >= 8) {
+            if (main.VoteNumber.get(p) >= 7) {
                 main.VoteNumber.put(p, 1);
             }
 
@@ -46,6 +45,8 @@ public class VoteCommand implements CommandExecutor {
                     book.setItemMeta(meta);
                 //ouverture du book
                     main.openBook(p,book);
+                //si le book marche pas :
+                p.sendMessage("§6§lVote #1 : " + "https://www.serveurs-minecraft.org/vote.php?id=60934");
 
             }else if (main.VoteNumber.get(p) == 2) {
                 System.out.println(p.getName() + "à effectué le vote : 2");
@@ -59,6 +60,8 @@ public class VoteCommand implements CommandExecutor {
                 book.setItemMeta(meta);
                 //ouverture du book
                 main.openBook(p,book);
+                //si le book marche pas :
+                p.sendMessage("§6§lVote #2 : " + "https://serveur-minecraft.com/2598");
 
             }else if (main.VoteNumber.get(p) == 3) {
                 System.out.println(p.getName() + "à effectué le vote : 3");
@@ -72,6 +75,8 @@ public class VoteCommand implements CommandExecutor {
                 book.setItemMeta(meta);
                 //ouverture du book
                 main.openBook(p,book);
+                //si le book marche pas :
+                p.sendMessage("§6§lVote #3 : " + "https://serveurs-mc.net/index.php/serveur/251");
 
             }else if (main.VoteNumber.get(p) == 4) {
                 System.out.println(p.getName() + "à effectué le vote : 4");
@@ -85,6 +90,8 @@ public class VoteCommand implements CommandExecutor {
                 book.setItemMeta(meta);
                 //ouverture du book
                 main.openBook(p,book);
+                //si le book marche pas :
+                p.sendMessage("§6§lVote #4 : " + "https://top-serveurs.net/minecraft/vote/noskillworld");
 
             }else if (main.VoteNumber.get(p) == 5) {
                 System.out.println(p.getName() + "à effectué le vote : 5");
@@ -98,6 +105,8 @@ public class VoteCommand implements CommandExecutor {
                 book.setItemMeta(meta);
                 //ouverture du book
                 main.openBook(p,book);
+                //si le book marche pas :
+                p.sendMessage("§6§lVote #5 : " + "https://serveur-prive.net/minecraft/noskillworld-9550/vote");
 
             }else if (main.VoteNumber.get(p) == 6) {
                 System.out.println(p.getName() + "à effectué le vote : 1");
@@ -111,25 +120,10 @@ public class VoteCommand implements CommandExecutor {
                 book.setItemMeta(meta);
                 //ouverture du book
                 main.openBook(p,book);
-
-            }else if (main.VoteNumber.get(p) == 7) {
-                System.out.println(p.getName() + "à effectué le vote : 1");
-                //création du book
-                BookMeta meta = (BookMeta) Bukkit.getItemFactory().getItemMeta(Material.WRITTEN_BOOK);
-                assert meta != null;
-                meta.setTitle("Votez pour NoSkillworld !");
-                meta.setAuthor("Bobby");
-                meta.setPages("7");
-                ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
-                book.setItemMeta(meta);
-                //ouverture du book
-                main.openBook(p,book);
+                //si le book marche pas :
+                p.sendMessage("§6§lVote #6 : " + "https://www.liste-serveurs-minecraft.org/serveur-minecraft/noskillworld/");
 
             }
-
-
-
-
         }
 
 
